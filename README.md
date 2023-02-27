d1d3be5376ca53efda696d13d3cdaa549d851868
chennan.wei <kinglisky@icloud.com>
2023-02-27 18:54:31 +0800

    feat: update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# 写给小弥同学的生活脚本
+# 小弥同学和我的生活脚本
 
 -   [m3u8.download](./scripts/m3u8.download) m3u8 视频下载脚本
 -   [image.info.check](./scripts/image.info.check) 检查班级核酸信息
---
+++ scripts/zcool.download/index.js
@@ -0,0 +1,8 @@
+const Crawler = require('crawler');
+const download = require('download');
+
+const HOME_PAGE_URL = 'https://www.zcool.com.cn/u/13389811';
+
+// https://www.zcool.com.cn/u/13389811?p=1
+
+(async function () {})();
